5fa43e4549727d02f6a79075da8686b05804f9f4
ramrami-S <developer.venice@gmail.com>
2024-03-08 09:34:32 +0900

    Update signup.js

--- front/singup.js
+++ front/singup.js
@@ -1,56 +1,117 @@
-let name, gender, birthday, hometown, id, password;
+class Signup {
+    #app
+    #name;
+    #gender;
+    #birthday
+    #hometown
+    #id
+    #password;
+    
+    constructor(app) {
+        this.#app = app;
+        this.#name = "";
+        this.#gender = 0;
+        this.#birthday = "";
+        this.#hometown = "";
+        this.id = "";
+        this.#password = "";
 
-let nameInput = document.getElementById("name");
-let femaleChecked = document.getElementById("female");
-let maleChecked = document.getElementById("male");
-let birthdayInput = document.getElementById("birthday");
-let hometownInput = document.getElementById("hometown");
-let idInput = document.getElementById("id");
-let passwordInput = document.getElementById("password");
+        this.onChangeName();
+        this.onChangeBirthDay();
+        this.onChangeHometown();
+        this.onChangeId();
+        this.onChangePassword();
+        
+        this.onCheckFemale();
+        this.onCheckMale();
 
-nameInput.addEventListener("change", (e) => {
-  name = e.target.value;
-});
-femaleChecked.addEventListener("change", (e) => {
-  gender = 0;
-});
-maleChecked.addEventListener("change", (e) => {
-  gender = 0;
-});
-birthdayInput.addEventListener("change", (e) => {
-  birthday = e.target.value;
-});
-hometownInput.addEventListener("change", (e) => {
-  hometown = e.target.value;
-});
-idInput.addEventListener("change", (e) => {
-  id = e.target.value;
-});
-passwordInput.addEventListener("change", (e) => {
-  password = e.target.value;
-});
+        this.signup();
+        this.goSignin();
+    }
 
-let signupButton = document.getElementById("signup-button");
-signupButton.addEventListener("click", () => {
-  event.preventDefault();
-  console.log(
-    "이름: " +
-      name +
-      "\n성별: " +
-      (gender === 0 ? "여자" : "남자") +
-      "\n생일: " +
-      birthday +
-      "\n고향: " +
-      hometown +
-      "\n아이디: " +
-      id +
-      "\n비밀 번호: " +
-      password
-  );
-});
+    onChangeName() {
+        let input = this.#app.getElementById("name");
+        input.addEventListener("change", (e) => {
+            this.#name = e.target.value;
+        });
+    }
 
-let goSigninButton = document.getElementById("go-signin");
-goSigninButton.addEventListener("click", () => {
-  event.preventDefault();
-  console.log("로그인 페이지로 이동합니다.");
-});
+    onChangeBirthDay() {
+        let input = this.#app.getElementById("birthday");
+        input.addEventListener("change", (e) => {
+            this.#birthday = e.target.value;
+        });
+    }
+
+    onChangeHometown() {
+        let input = this.#app.getElementById("hometown");
+        input.addEventListener("change", (e) => {
+            this.#hometown = e.target.value;
+        });
+    }
+
+    onChangeId() {
+        let input = this.#app.getElementById("id");
+        input.addEventListener("change", (e) => {
+            this.#id = e.target.value;
+        });
+    }
+
+    onChangePassword() {
+        let input = this.#app.getElementById("password");
+        input.addEventListener("change", (e) => {
+            this.#password = e.target.value;
+        });
+    
+    }
+
+    onCheckFemale() {
+        let check = this.#app.getElementById("female");
+        check.addEventListener("change", () => {
+          this.#gender = 0;
+        });
+    }
+
+    onCheckMale() {
+        let check = this.#app.getElementById("male");
+        check.addEventListener("change", () => {
+          this.#gender = 1;
+        });
+    }
+
+    signup() {
+        let button = this.#app.getElementById("signup-button");
+        button.addEventListener("click", (e) => {
+          e.preventDefault();
+          console.log(
+            "이름: " +
+              this.#name +
+              "\n성별: " +
+              (this.#gender === 0 ? "여자" : "남자") +
+              "\n생일: " +
+              this.#birthday +
+              "\n고향: " +
+              this.#hometown +
+              "\n아이디: " +
+              this.#id +
+              "\n비밀 번호: " +
+              this.#password
+          );
+        });
+
+    }
+
+    goSignin(){
+        let button = this.#app.getElementById("go-signin");
+        button.addEventListener("click", (e) => {
+          e.preventDefault();
+          this.navigateTo("로그인 페이지");
+        });
+    }
+
+    navigateTo(page) {
+        console.log(`${page}로 이동합니다.`); 
+    }
+}
+
+s = new Signup(document);
