3a92cf1964f1d70643f14c6ac41e244de4e01920
ramrami-B <wislely66@gmail.com>
2024-03-20 23:40:15 +0900

    feat: 1주차 과제 refactor(여기까진 참고 가능)

--- front/src/js/signin.js
+++ front/src/js/signin.js
@@ -1,51 +1,25 @@
+// function signIn() {} -> 함수형 컴포넌트
+// 클래스형 컴포넌트 => 차이는 궁금하면 검색
 class SignIn {
   #app;
-  #id;
-  #password;
+  loginId;
+  password;
 
   constructor(app) {
     this.#app = app;
-    this.#id = "";
-    this.#password = "";
+    this.loginId = "";
+    this.password = "";
 
-    this.onChangeId();
-    this.onChangePassword();
     this.login();
-    this.goSignup();
-  }
-
-  onChangeId() {
-    let idInput = this.#app.getElementById("id");
-    idInput.addEventListener("change", (e) => {
-      this.#id = e.target.value;
-    });
-  }
-
-  onChangePassword() {
-    let passwordInput = this.#app.getElementById("password");
-    passwordInput.addEventListener("change", (e) => {
-      this.#password = e.target.value;
-    });
   }
 
   login() {
-    let form = this.#app.getElementsByTagName("form")[0];
-    form.addEventListener("submit", (e) => {
+    this.#app.getElementById("signinForm").addEventListener("submit", (e) => {
       e.preventDefault();
-      console.log("id: " + this.#id + " password: " + this.#password);
+      this.loginId = this.#app.getElementById("loginId").value;
+      this.password = this.#app.getElementById("password").value;
+      console.log("id: " + this.loginId + " password: " + this.password);
     });
-  }
-
-  goSignup() {
-    let goSignupButton = this.#app.getElementById("go-signup");
-    goSignupButton.addEventListener("click", (e) => {
-      e.preventDefault();
-      this.navigateTo("회원가입 페이지");
-    });
-  }
-
-  navigateTo(page) {
-    console.log(`${page}로 이동합니다.`);
   }
 }
 
--- front/src/js/singup.js
+++ front/src/js/singup.js
@@ -1,114 +1,51 @@
 class Signup {
   #app;
-  #name;
-  #gender;
-  #birthday;
-  #hometown;
-  #id;
-  #password;
+  name;
+  gender;
+  birthday;
+  hometown;
+  loginId;
+  password;
 
   constructor(app) {
     this.#app = app;
-    this.#name = "";
-    this.#gender = 0;
-    this.#birthday = "";
-    this.#hometown = "";
-    this.id = "";
-    this.#password = "";
-
-    this.onChangeName();
-    this.onChangeBirthDay();
-    this.onChangeHometown();
-    this.onChangeId();
-    this.onChangePassword();
-
-    this.onCheckFemale();
-    this.onCheckMale();
+    this.name = "";
+    this.gender = 0;
+    this.birthday = "";
+    this.hometown = "";
+    this.loginId = "";
+    this.password = "";
 
     this.signup();
-    this.goSignin();
-  }
-
-  onChangeName() {
-    let input = this.#app.getElementById("name");
-    input.addEventListener("change", (e) => {
-      this.#name = e.target.value;
-    });
-  }
-
-  onChangeBirthDay() {
-    let input = this.#app.getElementById("birthday");
-    input.addEventListener("change", (e) => {
-      this.#birthday = e.target.value;
-    });
-  }
-
-  onChangeHometown() {
-    let input = this.#app.getElementById("hometown");
-    input.addEventListener("change", (e) => {
-      this.#hometown = e.target.value;
-    });
-  }
-
-  onChangeId() {
-    let input = this.#app.getElementById("id");
-    input.addEventListener("change", (e) => {
-      this.#id = e.target.value;
-    });
-  }
-
-  onChangePassword() {
-    let input = this.#app.getElementById("password");
-    input.addEventListener("change", (e) => {
-      this.#password = e.target.value;
-    });
-  }
-
-  onCheckFemale() {
-    let check = this.#app.getElementById("female");
-    check.addEventListener("change", () => {
-      this.#gender = 0;
-    });
-  }
-
-  onCheckMale() {
-    let check = this.#app.getElementById("male");
-    check.addEventListener("change", () => {
-      this.#gender = 1;
-    });
   }
 
   signup() {
-    let button = this.#app.getElementById("signup-button");
-    button.addEventListener("click", (e) => {
+    this.#app.getElementById("signupForm").addEventListener("submit", (e) => {
       e.preventDefault();
+      this.name = this.#app.getElementById("name").value;
+      this.gender = this.#app.getElementById("female").checked
+        ? "female"
+        : "male";
+      this.birth = this.#app.getElementById("birth").value;
+      this.hometown = this.#app.getElementById("hometown").value;
+      this.loginId = this.#app.getElementById("loginId").value;
+      this.password = this.#app.getElementById("password").value;
+
       console.log(
         "이름: " +
-          this.#name +
+          this.name +
           "\n성별: " +
-          (this.#gender === 0 ? "여자" : "남자") +
+          this.gender +
           "\n생일: " +
-          this.#birthday +
+          this.birthday +
           "\n고향: " +
-          this.#hometown +
+          this.hometown +
           "\n아이디: " +
-          this.#id +
+          this.loginId +
           "\n비밀 번호: " +
-          this.#password
+          this.password
       );
     });
-  }
-
-  goSignin() {
-    let button = this.#app.getElementById("go-signin");
-    button.addEventListener("click", (e) => {
-      e.preventDefault();
-      this.navigateTo("로그인 페이지");
-    });
-  }
-
-  navigateTo(page) {
-    console.log(`${page}로 이동합니다.`);
   }
 }
 
